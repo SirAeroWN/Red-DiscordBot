7c6af547fbd8919e9322991cda57e3ceccd0b22f
norv <sicklyserver@gmail.com>
2019-10-09 22:07:39 -0400

    Strip it doooown

--- red.py
+++ red.py
@@ -581,8 +581,7 @@ def set_cog(cog, value):  # TODO: move this out of red.py
 
 
 def load_cogs(bot):
-    defaults = ("alias", "audio", "customcom", "downloader", "economy",
-                "general", "image", "mod", "streams", "trivia")
+    defaults = ("trivia")
 
     try:
         registry = dataIO.load_json("data/red/cogs.json")
